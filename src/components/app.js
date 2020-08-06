7fb95477c01f23ee868c4608ad380a48fd4b32e2
DaliaMatus <daliamatus@gmail.com>
2020-08-05 21:56:57 -0500

    creation of products and their styles and create a folder for the images

--- src/components/app.js
+++ src/components/app.js
@@ -18,7 +18,67 @@ export default class App extends Component {
           <ul className='app__main-products'>
             <li>
               <div className='app__main-product'>
-                 <img src='' alt='product'/>
+                 <img src='/images/sudadera-blanca.jpg' alt='product'/>
+                 <div className='product-name'>
+                   <a href='product.js'>Harry Potter Sweatshirt</a>
+                  </div>
+                 <div className='product-brand'>Mundo Harry Potter</div>
+                 <div className='product-price'>$4.37</div>
+                 <div className='product-rating'>6.1 Stars (21 Reviews)</div>
+                 <h3>Sold</h3>
+                 <progress className='product-progress' min='0' max='100' value='25'></progress>
+              </div>
+            </li>
+            <li>
+              <div className='app__main-product'>
+                 <img src='/images/sudadera-blanca.jpg' alt='product'/>
+                 <div className='product-name'>
+                   <a href='product.js'>Harry Potter Sweatshirt</a>
+                  </div>
+                 <div className='product-brand'>Mundo Harry Potter</div>
+                 <div className='product-price'>$4.37</div>
+                 <div className='product-rating'>6.1 Stars (21 Reviews)</div>
+                 <h3>Sold</h3>
+                 <progress className='product-progress' min='0' max='100' value='12'></progress>
+              </div>
+            </li>
+            <li>
+              <div className='app__main-product'>
+                 <img src='/images/sudadera-blanca.jpg' alt='product'/>
+                 <div className='product-name'>
+                   <a href='product.js'>Harry Potter Sweatshirt</a>
+                  </div>
+                 <div className='product-brand'>Mundo Harry Potter</div>
+                 <div className='product-price'>$4.37</div>
+                 <div className='product-rating'>6.1 Stars (21 Reviews)</div>
+                 <h3>Sold</h3>
+                 <progress className='product-progress' min='0' max='100' value='78'></progress>
+              </div>
+            </li>
+            <li>
+              <div className='app__main-product'>
+                 <img src='/images/sudadera-blanca.jpg' alt='product'/>
+                 <div className='product-name'>
+                   <a href='product.js'>Harry Potter Sweatshirt</a>
+                  </div>
+                 <div className='product-brand'>Mundo Harry Potter</div>
+                 <div className='product-price'>$4.37</div>
+                 <div className='product-rating'>6.1 Stars (21 Reviews)</div>
+                 <h3>Sold</h3>
+                 <progress className='product-progress' min='0' max='100' value='36'></progress>
+              </div>
+            </li>
+            <li>
+              <div className='app__main-product'>
+                 <img src='/images/sudadera-blanca.jpg' alt='product'/>
+                 <div className='product-name'>
+                   <a href='product.js'>Harry Potter Sweatshirt</a>
+                  </div>
+                 <div className='product-brand'>Mundo Harry Potter</div>
+                 <div className='product-price'>$4.37</div>
+                 <div className='product-rating'>6.1 Stars (21 Reviews)</div>
+                 <h3>Sold</h3>
+                 <progress className='product-progress' min='0' max='100' value='52'></progress>
               </div>
             </li>
           </ul>
