03c79403a594b82d0bd1f751f9fdb1579240662b
DaliaMatus <daliamatus@gmail.com>
2020-08-05 21:38:15 -0500

    create the app.js base structure and styles

--- src/components/app.js
+++ src/components/app.js
@@ -3,10 +3,32 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return(
-      <div>
-        <h1>App</h1>
+      <div className='app'>
+        <header className='app__header'>
+          <div className='app__header-first'>
+            <a href='app.js'>Sweatshirts</a>
+          </div>
+          <div className='app__header-second'>
+            <a href='cart.js'>Cart</a>
+            <a href='signin.js'>Sign in</a>
+          </div>
+        </header>
+        <main className='app__main'>
+          <div className='app__main-content'>
+          <ul className='app__main-products'>
+            <li>
+              <div className='app__main-product'>
+                 <img src='' alt='product'/>
+              </div>
+            </li>
+          </ul>
+          </div>
+        </main>
+        <footer className='app__footer'>
+          ©1997-2020  All right reserved
+        </footer>        
       </div>
-    )
+    );
   }
 }
 
