9bc285ac0030f467a3a23d06a7348cafda3a853e
DaliaMatus <daliamatus@gmail.com>
2020-08-08 18:49:40 -0500

    I installed redux, react-redux and redux-thunk; I created the component store.js and the folders reducers and constants, with their respective files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+
 import './style/main.scss';
 import App from './components/app';
+import store from './components/store';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
