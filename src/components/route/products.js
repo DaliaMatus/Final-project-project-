ce3b8a6a62cbc04eb637c9f5f5980b903a91e76b
DaliaMatus <daliamatus@gmail.com>
2020-08-10 17:56:53 -0500

    implemented Redux in the Product.js component, created a variable for the quantity and modified the quantity JSX to remove the static quantity

--- src/components/route/products.js
+++ src/components/route/products.js
@@ -1,64 +1,83 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {Link} from "react-router-dom";
+import{useSelector, useDispatch} from 'react-redux';
 
-import data from '../database';
+import { detailsProduct } from '../../actions/productActions';
 
 function Products(props) {
-    const product=data.products.find(x=> x._id === props.match.params.id);
+    const [qty, setQty]=useState(1);
+    const productDetails = useSelector(state =>state.productDetails);
+    const{product, loading,error}= productDetails;
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+        dispatch(detailsProduct(props.match.params.id));
+        return ()=>{
+            //
+        };
+    }, [])
         return (
             <div>
                 <div className="back-to-result">
                     <Link to="/">Back to Result</Link>
                 </div>
-                <div className="products__details">
-                    <div className="details-image">
-                        <img src={product.image} alt='product'/>
-                    </div>
-                    <div className="details-info">
-                        <ul>
-                            <li>
-                                <h1>{product.name}</h1>
-                            </li>
-                            <li>
-                                {product.stars} Stars ({product.reviews} Reviews)
-                            </li>
-                            <li className="product-price">
-                                ${product.price}
-                            </li>
-                            <li>
-                                Description:
-                                <div>
-                                    {product.description}
-                                </div>
-                            </li>
-                            
-                        </ul>
-                    </div>
-                    <div className='details-action'>
-                        <ul>
-                           <li className="product-price">
-                               ${product.price}
-                            </li> 
-                            <li>
-                               Status:{product.status}
-                            </li> 
-                            <li>                              
-                                Quantity: <select>
-                                    <option>1</option>
-                                    <option>2</option>
-                                    <option>3</option>
-                                    <option>4</option>
-                                    <option>5</option>
-                                </select>
-                            </li>
-                            <li>
-                                <button>Add to Cart</button>
-                            </li> 
-                        </ul>
-                    </div>
 
-                </div>
+                {loading ? <div className='loading'>
+                        <div className='loading1'></div>
+                        <div className='loading2'></div>
+                        </div> :
+                    error ? <div>{error}</div> :
+                        (
+                            <div className="products__details">
+                                <div className="details-image">
+                                    <img src={product.image} alt='product'/>
+                                </div>
+                                <div className="details-info">
+                                    <ul>
+                                        <li>
+                                            <h1>{product.name}</h1>
+                                        </li>
+                                        <li>
+                                            {product.stars} Stars ({product.reviews} Reviews)
+                                        </li>
+                                        <li className="product-price">
+                                            ${product.price}
+                                        </li>
+                                        <li>
+                                            Description:
+                                            <div>
+                                                {product.description}
+                                            </div>
+                                        </li>
+                                        
+                                    </ul>
+                                </div>
+                                <div className='details-action'>
+                                    <ul>
+                                        <li className="product-price">
+                                        ${product.price}
+                                        </li> 
+                                        <li>
+                                        Status:{product.status}
+                                        </li> 
+                                        <li>                              
+                                            Quantity: <select value={qty} onChange={(e) =>{setQty(e.target.value)}}>
+                                                {[...Array(product.countInStock).keys()].map(x=>
+                                                   <option key={x+1} value={x+1}>{x+1}</option> 
+                                                    )}
+                                            </select>
+                                        </li>
+                                        <li>
+                                            <button>Add to Cart</button>
+                                        </li> 
+                                    </ul>
+                                </div>
+
+                            </div>
+                        )                    
+                }
             </div>
         )
     }
+
 export default Products;
