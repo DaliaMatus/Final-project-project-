306a78e5f3b4abbb9868a06e5ec606b5dc5558c1
DaliaMatus <daliamatus@gmail.com>
2020-08-12 21:24:36 -0500

    I resized the images in product.scss, added Reviews and removed the import from useState in home.js

--- src/components/route/home.js
+++ src/components/route/home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import {Link} from "react-router-dom";
 import {useSelector, useDispatch}from 'react-redux';
 import {listProducts} from '../../actions/productActions';
@@ -34,7 +34,7 @@ function Home(props) {
                     </div>
                     <div className='product-brand'>{product.brand}</div>
                     <div className='product-price'>${product.price}</div>
-                    <div className='product-rating'>{product.stars} Stars ({product.reviews})</div>
+                    <div className='product-rating'>{product.stars} Stars ({product.reviews} Reviews)</div>
                     <h3>Sold</h3>
                     <progress className='product-progress' min='0' max='100' value='25'></progress>
                   </div>
